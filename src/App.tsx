5bc7d69cc95a61821bb2a337fb93a7332a2d162f
Mark Koelle <mkoelle@gmail.com>
2022-02-26 15:55:54 -0600

    restore navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './placeholder.png';
 import './App.css';
 import 'bulma/css/bulma.min.css'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" data-testid='logo' />
-      </header>
-      <main> 
+      <main>
+        <Navbar></Navbar>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" data-testid='logo' />
+        </header>
       </main>
     </div>
   );
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import logo from '../../assets/logo.png'
+
+function App() {
+  return (
+    <React.Fragment>
+    <nav className="navbar topNav">
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="#">
+            <img src={logo} width="28" height="28" />
+            <p className="title is-3 nav-title">Library</p>
+          </a>
+          <div
+            className="navbar-burger burger"
+            // @click="showNav = !showNav"
+            data-target="topNav"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div id="topNav" className="navbar-menu" > 
+         {/* :className="{ 'is-active': showNav }"> */}
+          <div className="navbar-start">
+            <a className="navbar-item" href="#">Home</a>
+            <a className="navbar-item" href="#">Tabs</a>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+                <p className="control">
+                  <a className="button is-small">
+                    <span className="icon">
+                      <i className="fa fa-user-plus"></i>
+                    </span>
+                    <span>Register</span>
+                  </a>
+                </p>
+                <p className="control">
+                  <a className="button is-small login">
+                    <span className="icon">
+                      <i className="fa fa-user"></i>
+                    </span>
+                    <span>Login</span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <nav className="navbar is-hidden-mobile is-hidden-tablet-only">
+      <div className="container">
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item is-active" href="#">Popular</a>
+            <a className="navbar-item" href="#">Recent</a>
+            <a className="navbar-item" href="#">Rising</a>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <input
+                className="input"
+                type="search"
+                placeholder="Search library..."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </React.Fragment>
+  );
+}
+
+export default App;
