92b629f80b2b0c17c0ecad677cb104c4a6e28add
Mark Koelle <mkoelle@gmail.com>
2022-03-02 10:34:07 -0600

    Update images

--- src/components/lendables/Lendables.tsx
+++ src/components/lendables/Lendables.tsx
@@ -4,14 +4,14 @@ const MOCK_LENDABLES = [
     {
         "name": "Item 1",
         "lender": "user",
-        "image": "./img/placeholder.png",
+        "image": "./img/LendersLibrary.png",
         "tags": ["Board Game"],
         "description": "Ut enim ad minim veniam Mandibuzz Braviary Pidgeotto Azelf Bronzong Vine Whip. Glitch City Rotom Machop Carvanha Koffing Koffing Relicanth Quagsire. Marsh Badge Croconaw Corphish Audino Hitmonchan Mew Pikachu. Charmeleon ex ea commodo Silcoon Magnemite I like shorts Seaking Minccino. V for victory Cacturne Badge Arcanine Sealeo Houndour Phanpy."
     },
     {
         "name": "Item 2",
         "lender": "bob",
-        "image": "./img/placeholder.png",
+        "image": "./img/LendersLibrary.png",
         "tags": ["Space Travel", "Vehicle"],
         "description": "Hive Badge Dusknoir Venomoth Nuzleaf Dragon Rage Hive Badge Harden. Lorem ipsum dolor sit amet Keldeo Rage Unown Shiftry you're not wearing shorts Virizion. Yellow Goldeen Grass Floatzel Kanto Camerupt Graveler. Flying Dewott Gliscor Lilligant Brock Misdreavus Pidgeotto. Water Gun Rotom Litwick Donphan Wurmple Nincada Noctowl."
     }
