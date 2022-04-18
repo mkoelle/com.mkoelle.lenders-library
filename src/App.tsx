eecb89e76a4bf19ff7d18354da2caeb7160a97a0
Mark Koelle <mkoelle@gmail.com>
2022-04-18 17:04:48 -0500

    use sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom'
-import './App.css';
-import 'bulma/css/bulma.min.css'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import AllLendables from './components/pages/AllLendables'
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bulma/bulma.sass'
+import './assets/main.scss'
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
