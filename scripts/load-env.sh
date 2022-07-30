0f9c8831032bddaa81eb3f282693d7245acae881
Mark Koelle <mkoelle@gmail.com>
2022-07-30 18:10:30 -0500

    pull app key and client id independently

--- scripts/load-env.sh
+++ scripts/load-env.sh
@@ -3,7 +3,9 @@ set +x #xtrace
 set +v #verbose
 
 # Loads dynamic react parameters before build or start
-export REACT_APP_USER_POOL=${$(aws cloudformation list-exports --query "Exports[?Name=='com-mkoelle-ll-auth-userPool'].Value" --output text):-'us-east-1_X5xXXXx6X'}
+app_key=$(aws cloudformation list-exports --query "Exports[?Name=='com-mkoelle-ll-auth-userPool'].Value" --output text)
+export REACT_APP_USER_POOL=${app_key:-'us-east-1_X5xXXXx6X'}
 echo $REACT_APP_USER_POOL
-export REACT_APP_CLIENT_ID=${$(aws cloudformation list-exports --query "Exports[?Name=='com-mkoelle-ll-auth-clientId'].Value" --output text):-'45xx55xxxxxxxxxx6xx444xxxx'}
+client_id=$(aws cloudformation list-exports --query "Exports[?Name=='com-mkoelle-ll-auth-clientId'].Value" --output text)
+export REACT_APP_CLIENT_ID=${client_id:-'45xx55xxxxxxxxxx6xx444xxxx'}
 echo $REACT_APP_CLIENT_ID
